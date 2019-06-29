e78ba4d6d603f99bafc53036137a8e692d40626d
蓝萱 <lanxuan@lanxuandeMacBook-Pro.local>
2019-06-29 20:14:09 +0800

    hello-world

---
+++ hello-world.py
@@ -0,0 +1,21 @@
+print('I\'m ok.')
+a=3
+b=4
+if a==2:
+    print(3)
+else:
+    print('piggy')
+print('''line1... \nline2
+... line3''')
+print(r'''hello,\n
+world''')
+age=28
+if age >= 18:
+    print('adult')
+else:
+    print('teenager')
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print(s3)
+print(s4)
