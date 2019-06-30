74d56c61d0a2cf942b3a78901c2503799a864fba
蓝萱 <lanxuan@lanxuandeMacBook-Pro.local>
2019-06-30 21:21:34 +0800

    update

--- dict.py
+++ dict.py
@@ -1,12 +1,17 @@
 def lx_sum(my_dict):
     sum=0
-    for k,v in d.items():
+    for k,v in my_dict.items():
         sum=sum+v
     return sum
 
+def lx_len(my_dict):
+    sum=0
+    for k,v in my_dict.items():
+        sum=sum+1
+    return sum
+
 d = {'piggy': 23, 'sheepy': 30, 'tiger': 45,'chicken':50}
-n=len(d)
+n=lx_len(d)
 a=lx_sum(d)/n
 s='average:%s' %(a)
-print(s)
-    
+print(s) 
